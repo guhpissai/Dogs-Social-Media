652cf35db567c82db817c5c4216501012fc87c87
guhpissai <gustavopissai3d@gmail.com>
2023-09-14 16:19:41 -0300

    Implementando um fetch GET para o usuario

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,13 +3,25 @@ import { Link } from "react-router-dom";
 import Input from "../Form/Input";
 import Button from "../Form/Button";
 import useForm from "../../Hooks/useForm";
-import { TOKEN_POST } from "../../api";
+import { TOKEN_POST, USER_GET } from "../../api";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(event) {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) getUser(token);
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
@@ -18,12 +30,10 @@ const LoginForm = () => {
         password: password.value,
       });
 
-      fetch(url, options)
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => console.log(json));
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
     }
   }
 
