3f3a72c53fcb64cec84dbfcfae7ba863e496265c
guhpissai <gustavopissai3d@gmail.com>
2023-09-14 12:52:44 -0300

    Resetando css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login / Criar</Link>
-    </nav>
+    <div className={styles.header}>
+      <nav className="container">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Criar</Link>
+      </nav>
+    </div>
   );
 };
 
