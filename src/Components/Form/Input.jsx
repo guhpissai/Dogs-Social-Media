1aab2c8144560b04369a8b14eced5d5a1b2c389c
guhpissai <gustavopissai3d@gmail.com>
2023-09-14 15:55:07 -0300

    Criando validacoes dos valores nos inputs e implementando o fetch para resgatar o token

--- src/Components/Form/Input.jsx
+++ src/Components/Form/Input.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, onBlur, error }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} className={styles.input} type={type} />
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
