efb34d36f09396034fb5543457ab498dd08ab8bc
guhpissai <gustavopissai3d@gmail.com>
2023-09-15 17:38:17 -0300

    Definindo a rota conta como uma rota protegida

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -37,7 +37,6 @@ export const UserStorage = ({ children }) => {
       setLoading(true);
       const { url, options } = TOKEN_POST({ username, password });
       const tokenRes = await fetch(url, options);
-      console.log(tokenRes);
       if (!tokenRes.ok) throw new Error(`Usuário ou senha incorreto`);
       const { token } = await tokenRes.json();
       window.localStorage.setItem("token", token);
@@ -67,6 +66,8 @@ export const UserStorage = ({ children }) => {
         } finally {
           setLoading(false);
         }
+      } else {
+        setLogin(false);
       }
     }
     autoLogin();
