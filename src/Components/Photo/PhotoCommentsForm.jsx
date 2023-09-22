dda5d08c1317031ffb8ec5db2e0cca386210703f
guhpissai <gustavopissai3d@gmail.com>
2023-09-21 23:16:22 -0300

    Implementando: Componente para exibir os comentarios | Componente para adicionar comentarios | Estilos para os componentes relacionaods aos comentarios

---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
+import useFetch from "../../Hooks/useFetch";
+import { COMMENT_POST } from "../../api";
+import Error from "../Helper/Error";
+import styles from "./PhotoCommentsForm.module.css";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = React.useState("");
+  const { error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    setComment("");
+    if (response.ok) setComments((comments) => [...comments, json]);
+    <Error error={error} />;
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <textarea
+        className={styles.textarea}
+        onChange={({ target }) => setComment(target.value)}
+        value={comment}
+        name="comment"
+        id={id}
+      ></textarea>
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
